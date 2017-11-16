13abe3850348b25e2ea40783420e6ec7f6c40875
Renato Cruz <cruzrenato@gmail.com>
2017-11-15 16:39:51 -0800

    update version

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ keccak_hash_module = Extension('keccak_hash',
                                include_dirs=['.', './sha3'])
 
 setup (name = 'keccak_hash',
-       version = '1.4.1',
+       version = '1.4.2',
        maintainer = 'rc125',
        maintainer_email = 'rc125@protonmail.com',
        url = 'https://github.com/rc125/python-keccak_hash',
-       download_url = 'https://github.com/rc125/python-keccak_hash/archive/1.4.1.tar.gz',
+       download_url = 'https://github.com/rc125/python-keccak_hash/archive/1.4.2.tar.gz',
        description = 'Binding for keccak proof of work hashing.',
        ext_modules = [keccak_hash_module])
